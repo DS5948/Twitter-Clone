fad48976a3117e8e902802241a6887ae2662e568
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-01-12 23:36:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
+app.use(express.json({ limit: "10mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
