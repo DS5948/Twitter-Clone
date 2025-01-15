1716f10a09b4d2f6e109da5c479c5d93066e44a1
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-01-15 22:08:18 +0530

    Update Sidebar.jsx

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,5 +1,5 @@
 import XSvg from "../svgs/X";
-
+import { GoHomeFill } from "react-icons/go";
 import { GoHome } from "react-icons/go";
 import { GoBell } from "react-icons/go";
 import { AiOutlineUser } from "react-icons/ai";
@@ -8,6 +8,9 @@ import { IoIosLogOut } from "react-icons/io";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useEffect, useState } from "react";
+import { GoBellFill } from "react-icons/go";
+import { FaUser } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa6";
 
 const Sidebar = () => {
   const location = useLocation()
@@ -51,36 +54,37 @@ const Sidebar = () => {
   const { data: authUser } = useQuery({ queryKey: ["authUser"] });
 
   return (
-      <div className="fixed border-t border-gray-300 bottom-0 left-0 bg-white md:sticky md:top-0 md:left-0 md:h-screen flex gap-4 items-center md:items-start w-full md:flex-col md:max-w-60 md:w-fit p-1">
+      <div className="fixed border-t md:border-r border-gray-300 bottom-0 left-0 bg-white md:sticky md:top-0 md:left-0 md:h-screen flex gap-4 items-center md:items-start w-full md:flex-col md:max-w-60 md:w-fit p-1">
         <Link to="/" className="hidden md:flex justify-center md:justify-start">
           <XSvg className="px-2 w-12 h-12 rounded-full fill-black" />
         </Link>
         <ul className="flex justify-between items-center md:items-stretch w-full h-full md:flex-col gap-3 md:mt-4 p-1">
-          <li className={`flex ${active === 'home' ? 'bg-slate-300' : ''} hover:bg-slate-300 rounded-md justify-center md:justify-start`} >
+          <li className={`flex ${active === 'home' ? 'font-semibold' : ''} hover:bg-slate-300 rounded-md justify-center md:justify-start`} >
             <Link
               to="/"
               className="flex gap-3 items-center transition-all rounded-full duration-300 py-2 pl-2 pr-2 max-w-fit cursor-pointer"
             >
-              <GoHome size={24} />
+              {active === 'home' ? <GoHomeFill size={24}  /> : <GoHome size={24}/>}
+              
               <span className="text-lg hidden md:block">Home</span>
             </Link>
           </li>
-          <li className={`flex ${active === 'notifications' ? 'bg-slate-300' : ''} hover:bg-slate-300 rounded-md justify-center md:justify-start`} >
+          <li className={`flex ${active === 'notifications' ? 'font-semibold' : ''} hover:bg-slate-300 rounded-md justify-center md:justify-start`} >
             <Link
               to="/notifications"
               className="flex gap-3 items-center transition-all rounded-full duration-300 py-2 pl-2 pr-2 max-w-fit cursor-pointer"
             >
-              <GoBell size={24} />
+              {active === 'notifications' ? <GoBellFill size={24} /> : <GoBell size={24} />}
               <span className="text-lg hidden md:block">Notifications</span>
             </Link>
           </li>
 
-          <li className={`flex ${active === 'profile' ? 'bg-slate-300' : ''} hover:bg-slate-300 rounded-md justify-center md:justify-start`} >
+          <li className={`flex ${active === 'profile' ? 'font-semibold' : ''} hover:bg-slate-300 rounded-md justify-center md:justify-start`} >
             <Link
               to={`/profile/${authUser?.username}`}
               className="flex gap-3 items-center transition-all rounded-full duration-300 py-2 pl-2 pr-2 max-w-fit cursor-pointer"
             >
-              <AiOutlineUser size={24} />
+              {active === 'profile' ? <FaUser size={22}/> : <FaRegUser size={22} />}
               <span className="text-lg hidden md:block">Profile</span>
             </Link>
           </li>
