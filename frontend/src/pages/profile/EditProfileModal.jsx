661a4b07d1bec1cc3f961a0b61f8f4f52e6d9a7d
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-01-15 23:00:40 +0530

    Update EditProfileModal.jsx

--- frontend/src/pages/profile/EditProfileModal.jsx
+++ frontend/src/pages/profile/EditProfileModal.jsx
@@ -55,7 +55,7 @@ const EditProfileModal = ({ authUser }) => {
   />
 
   <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+    <div className="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
 	<DialogPanel
   transition
   className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
@@ -70,7 +70,7 @@ const EditProfileModal = ({ authUser }) => {
       />
       <div className="text-left">
         <h2 className="text-lg font-semibold">{authUser.fullName}</h2>
-        <p className="text-sm text-gray-500">{authUser.email}</p>
+        <p className="text-sm text-gray-500 text-wrap">{authUser.email}</p>
       </div>
     </div>
 
@@ -190,7 +190,7 @@ const EditProfileModal = ({ authUser }) => {
 </div>
 
  {/* Footer */}
- <div className="w-full py-3 sm:flex justify-between">
+ <div className="w-full py-3 flex justify-between">
     {/* <button
       type="button"
       onClick={() => alert("Delete User")}
@@ -202,14 +202,14 @@ const EditProfileModal = ({ authUser }) => {
       <button
         type="button"
         onClick={() => setOpen(false)}
-        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+        className=" inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mt-0 w-auto"
       >
         Cancel
       </button>
       <button
         type="submit"
         onClick={() => setOpen(false)}
-        className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 sm:ml-3 sm:w-auto"
+        className="inline-flex justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 ml-3 w-auto"
       >
         Save Changes
       </button>
