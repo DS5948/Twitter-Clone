21f846b97fb779f253d7fead3792fb63ac7eb68f
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-01-16 00:46:55 +0530

    Update ProfileHeaderSkeleton.jsx

--- frontend/src/components/skeletons/ProfileHeaderSkeleton.jsx
+++ frontend/src/components/skeletons/ProfileHeaderSkeleton.jsx
@@ -4,8 +4,6 @@ const ProfileHeaderSkeleton = () => {
 			<div className='flex gap-2 items-center'>
 				<div className='flex flex-1 gap-1'>
 					<div className='flex flex-col gap-1 w-full '>
-						<div className='skeleton h-4 w-12 rounded-full bg-gray-300'></div>
-						<div className='skeleton h-4 w-16 rounded-full bg-gray-300'></div>
 						<div className='skeleton h-40 w-full relative bg-gray-300'>
 							<div className='skeleton h-20 w-20 rounded-full border absolute -bottom-10 left-3 bg-gray-300'></div>
 						</div>
