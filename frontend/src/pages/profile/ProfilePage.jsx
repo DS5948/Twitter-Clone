8e51aa7dd1cea2248aa51a67e2102fa1a613417c
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-01-16 17:19:45 +0530

    Update ProfilePage.jsx

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -147,6 +147,7 @@ const ProfilePage = () => {
             </div>
             <div className="flex items-center justify-end px-4 mt-5">
               {isMyProfile && <EditProfileModal authUser={authUser} />}
+              {isMyProfile && 
               <button className='inline-flex justify-center px-3 py-2 text-sm font-semibold sm:ml-3'
                onClick={(e) => {
                 e.preventDefault()
@@ -154,6 +155,7 @@ const ProfilePage = () => {
                }}>
                 <IoIosLogOut size={30}/>
               </button>
+              }  
               {!isMyProfile && (
                 <button
 				  disabled={requested}
