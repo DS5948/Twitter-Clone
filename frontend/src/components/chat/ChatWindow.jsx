542e7a8f50a202e473f262852e5cf89115561753
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-06-15 21:54:37 +0530

    Update ChatWindow.jsx

--- frontend/src/components/chat/ChatWindow.jsx
+++ frontend/src/components/chat/ChatWindow.jsx
@@ -181,7 +181,7 @@ const ChatWindow = () => {
 
       <div className="relative flex-1 flex flex-col">
         {/* Header */}
-        <div className="flex items-center justify-between px-4 py-3 border-b border-gray-300">
+        <div className="w-full absolute top-0 left-0 sm:flex items-center justify-between px-4 py-3 border-b border-gray-300">
           <div className="flex items-center gap-3">
             <img
               src={getConversationImage()}
