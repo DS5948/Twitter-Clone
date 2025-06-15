68ec1328e10e358f66eef674f46df9c9981eb0d0
DS5948 <126962566+DS5948@users.noreply.github.com>
2025-06-15 14:01:54 +0530

    Update ChatWindow.jsx

--- frontend/src/components/chat/ChatWindow.jsx
+++ frontend/src/components/chat/ChatWindow.jsx
@@ -191,7 +191,7 @@ const ChatWindow = () => {
                     >
                       {msg.text || msg.caption || ""}
                     </div>
-                    {msg.isReadBy?.length > 0 && (
+{/*                     {msg.isReadBy?.length > 0 && (
                       <div className="flex mt-1 space-x-1">
                         {msg.isReadBy.map(
                           (user) =>
@@ -207,7 +207,7 @@ const ChatWindow = () => {
                                 title={user.fullName}
                               />
                             )
-                        )}
+                        )} */}
                       </div>
                     )}
                   </div>
